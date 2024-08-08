c9eedd2dcaa3665662abaa9c95d24a64c67a7524
evgeny <vaazhen@mail.ru>
2024-08-08 13:44:00 +0300

    это я изменил 04.08

--- src/main/java/ru/kata/spring/boot_security/demo/configs/SuccessUserHandler.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/SuccessUserHandler.java
@@ -17,9 +17,9 @@ public class SuccessUserHandler implements AuthenticationSuccessHandler {
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException {
         Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
         if (roles.contains("ROLE_ADMIN")) {
-            httpServletResponse.sendRedirect("/users.html");
+            httpServletResponse.sendRedirect("/admin/users.html");
         } else {
-            httpServletResponse.sendRedirect("/user");
+            httpServletResponse.sendRedirect("/user.html");
         }
     }
 }
